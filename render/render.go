2c086126554d15a762a0009d2a7704a74b420361
saatvik-10 <saatvikmadan529@gmail.com>
2025-02-15 18:38:09 +0530

    learned about layouts

--- render/render.go
+++ render/render.go
@@ -7,7 +7,7 @@ import (
 )
 
 func RenderTemplate(w http.ResponseWriter, html string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + html)
+	parsedTemplate, _ := template.ParseFiles("./templates/" + html, "./templates/base.layout.html")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template: ", err)
