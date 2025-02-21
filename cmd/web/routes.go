c6f32bebdb031b82859c771af11c5f66d6f36de9
saatvik-10 <saatvikmadan529@gmail.com>
2025-02-21 22:11:49 +0530

    learned go-chi router , middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,17 +1,27 @@
 package main
 
 import (
-	"github.com/bmizerany/pat"
+	//"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 	"web-with-go/pkg/config"
 	"web-with-go/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+//	mux := pat.New()
+//
+//	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+//	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
+
+	//middleware
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module web-with-go
 
 go 1.23.6
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+//to get rid of unused packages, use 'go mod tidy'
+
+require (
+	//github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+	github.com/go-chi/chi/v5 v5.2.1 // indirect
+)
