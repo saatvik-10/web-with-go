e175e561820550f3d15758a36d0d728bc1c97f42
saatvik-10 <saatvikmadan529@gmail.com>
2025-02-21 22:44:22 +0530

    learned creating middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,6 +19,7 @@ func routes(app *config.AppConfig) http.Handler {
 
 	//middleware
 	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
